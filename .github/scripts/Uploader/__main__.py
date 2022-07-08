a9765d72c9f0bf8adf1cd0c1ab590012091fed7c
Ashwin.D.S <astroashwin@outlook.com>
2022-07-08 18:27:52 +0000

    Rework Signed-off-by: Ashwin.D.S <astroashwin@outlook.com>

--- .github/scripts/Uploader/__main__.py
+++ .github/scripts/Uploader/__main__.py
@@ -1,3 +1,4 @@
+
 #!/usr/bin/env python
 #
 # Python code which automatically sends a Message in a Telegram Group
@@ -19,8 +20,13 @@
 
 import datetime
 import os
+import json
+import requests
+import hashlib
 
 SF_PASS = os.environ.get("SF_PASS")
+
+android_version_text = "pyro"
 
 try:
     new_tags = open("new_tags.txt", "r").readlines()
@@ -28,48 +34,127 @@ except Exception as e:
     new_tags = []
 
 try:
-    OTA = open("no_ota.txt", "r").readlines().replace("\n", "").replace(" ", "") == "no"
+    OTA = os.path.exists("no_ota.txt")
 except Exception as e:
-    OTA = True
+    OTA = False
+
+if OTA:
+    print("Pushing OTA")
+else:
+    print("Not Pushing OTA")
 
 cur_dir = os.getcwd()
 print(new_tags)
 for tag in new_tags:
     os.chdir(cur_dir + "/releases")
-    os.system("gh release download " + tag.replace("\n", ""))
+    os.system("gh release download " + tag.replace("\n", "") + " && ls")
+    print (cur_dir)
+    BIG_ROM_FILE = os.path.exists(cur_dir + "/releases/ROM_TOO_BIG.txt")
+    if BIG_ROM_FILE:
+        print("Big Zip")
+        BIG_FILE_INFO = open(cur_dir + "/releases/ROM_TOO_BIG.txt", "r").readlines()[0]
+        print (BIG_FILE_INFO)
+        ROM_MD5 = BIG_FILE_INFO.split(" ")[0]
+        ROM_NAME = BIG_FILE_INFO.split("/")[-1].replace("\n", "")
+        print (ROM_MD5 , ROM_NAME)
+        # If Size>2GB
+        print ("Combining bigger files to one ")
+        os.system("cat SparkOS.part?? > " + ROM_NAME)
+
+        try: 
+            with open(ROM_NAME, 'rb') as file_to_check:
+                data = file_to_check.read()    
+                md5_returned = hashlib.md5(data).hexdigest()
+
+            if ROM_MD5 == md5_returned:
+                print ("MD5 verified.")
+            else:
+                print ("MD5 verification failed!.")
+        except Exception as e:
+            print (e)
     recovery_path = ""
     for file in os.listdir(cur_dir + "/releases"):
         if file == "boot.img":
-            os.system("mv " + cur_dir + "/releases/boot.img " + cur_dir + "/releases/boot-" + tag.split("_")[0] + "-" + str(datetime.date.today()).replace("-", "") + ".img")
+            os.system("mv " + cur_dir + "/releases/boot.img " + cur_dir + "/releases/boot-" +
+                      tag.split("_")[0] + "-" + str(datetime.date.today()).replace("-", "") + ".img")
             break
         if file == "recovery.img":
-            os.system("mv " + cur_dir + "/releases/recovery.img " + cur_dir + "/releases/recovery-" + tag.split("_")[0] + "-" + str(datetime.date.today()).replace("-", "") + ".img")
+            os.system("mv " + cur_dir + "/releases/recovery.img " + cur_dir + "/releases/recovery-" +
+                      tag.split("_")[0] + "-" + str(datetime.date.today()).replace("-", "") + ".img")
             break
     print("Downloaded")
     os.chdir(cur_dir + "/releases-public")
     os.system("gh release create " + str(datetime.date.today()))
-    os.system("gh release upload " + str(datetime.date.today()) + " " + cur_dir + "/releases/*.zip")
-    os.system("gh release upload " + str(datetime.date.today()) + " " + cur_dir + "/releases/*.img")
-    
+    if not BIG_ROM_FILE:
+        os.system("gh release upload " + str(datetime.date.today()) +
+                  " " + cur_dir + "/releases/*.zip")
+    os.system("gh release upload " + str(datetime.date.today()) +
+              " " + cur_dir + "/releases/*.img")
+
     ROM_ZIP_NAME = "none"
 
     for file in os.listdir(cur_dir + "/releases/"):
         if file.endswith("gapps.zip"):
             ROM_ZIP_NAME = file
 
+    for file in os.listdir(cur_dir + "/releases/"):
+        if file.endswith(".json"):
+            device = file.replace(".json", "")
+            if not BIG_ROM_FILE:
+                github_download_link = "https://github.com/PixelOS-Releases/SparkOS-releases-public/releases/download/" + str(datetime.date.today()) + "/" + ROM_ZIP_NAME
+            else: 
+                # GH releases will not work over 2GB, so just use SF
+                github_download_link = "https://sourceforge.net/projects/sparkosofficial/files/" + device + "/" + ROM_ZIP_NAME
+            mjson = open(cur_dir + "/releases/" + file, "r").read().replace("GITHUB_RELEASES_PLACEHOLDER", github_download_link)
+            open(cur_dir + "/releases/" + file, "w+").write(mjson)
+
     if OTA:
         for file in os.listdir(cur_dir + "/releases/"):
             if file.endswith(".json"):
                 device = file.replace(".json", "")
-                json = open(cur_dir + "/releases/" + file, "r").read().replace("URL_PLACEHOLDER", "https://sourceforge.net/projects/sparkosofficial/files/" + device + "/" + ROM_ZIP_NAME)
-                open(cur_dir + "/releases/" + file, "w+").write(json)
+                mjson = open(cur_dir + "/releases/" + file, "r").read().replace("URL_PLACEHOLDER",
+                                                                               "https://sourceforge.net/projects/sparkosofficial/files/" + device + "/" + ROM_ZIP_NAME)
+                open(cur_dir + "/releases/" + file, "w+").write(mjson)
+
+    else:
+        for file in os.listdir(cur_dir + "/releases/"):
+            if file.endswith(".json"):
+                device = file.replace(".json", "")
+                updaterInfo = json.loads(requests.get("https://raw.githubusercontent.com/Spark-Devices/official_devices/" + android_version_text  + "/API/updater/" + device + ".json").content)
+                updaterInfo ["github_releases_url"] = "https://github.com/PixelOS-Releases/SparkOS-releases-public/releases/download/" + str(datetime.date.today()) + "/" + ROM_ZIP_NAME
+                open(cur_dir + "/releases/" + file, "w+").write(json.dumps(updaterInfo, indent=4), )
+
+
+
+    os.system("cp " + cur_dir + "/releases/*.json " + cur_dir + "/API/updater/")
+
+
+    push_commands = [
+        "git config --global user.name \"spkal01\"",
+        "git config --global user.email \"kalligeross@gmail.com\"",
+        "git fetch",
+        "git pull",
+        "git add .",
+        "git commit -m \"official_devices: update tags [no ci]\"",
+        "git push origin pyro",
+    ]
+
+    for command in push_commands:
+        os.system(command)
 
     os.system("sudo apt install sshpass")
     # os.system("mkdir ~/.ssh/")
     # os.system("ssh-keyscan frs.sourceforge.net >> ~/.ssh/known_hosts")
-    os.system("sshpass -p " + SF_PASS + " scp -o \"StrictHostKeyChecking no\" " + cur_dir + "/releases/*.img spkal01@frs.sourceforge.net:/home/frs/project/sparkosofficial/" + device + "")
-    os.system("sshpass -p " + SF_PASS + " scp -o \"StrictHostKeyChecking no\" " + cur_dir + "/releases/*.zip spkal01@frs.sourceforge.net:/home/frs/project/sparkosofficial/" + device + "")
 
-    os.system("cp " + cur_dir + "/releases/*.json " + cur_dir + "/API/updater/" )
+    try:
+        os.system("sshpass -p " + SF_PASS + " scp -o \"StrictHostKeyChecking no\" " + cur_dir +
+                  "/releases/*.img spkal01@frs.sourceforge.net:/home/frs/project/sparkosofficial/" + device + "/recovery")
+        os.system("sshpass -p " + SF_PASS + " scp -o \"StrictHostKeyChecking no\" " + cur_dir +
+                  "/releases/*.zip spkal01@frs.sourceforge.net:/home/frs/project/sparkosofficial/" + device + "")
+    except:
+        print("Something went wrong")
+
     print("Uploaded")
-    os.system("rm -rf " + cur_dir + "/releases/*.img " + cur_dir + "/releases/*.zip ")
+
+    os.system("rm -rf " + cur_dir + "/releases/*.img " +
+              cur_dir + "/releases/*.zip ")
